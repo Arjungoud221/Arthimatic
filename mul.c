e411ec631450ec24fcaa39c2c14ea8f4f3def69d
mallikharjunp <mallikharjun.palasa@gytechs.com>
2023-02-22 11:12:59 +0530

    first commit

---
+++ mul.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	int m = 17;
+	int h = 14;
+
+	printf("Multiplication of %d and %d is:%d",m,h,m*h);
+}
+
---
+++ sum.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 100 ;
+	int b = 90 ;
+
+	printf("the sum of %d and %d is :%d\n",a,b,a+b);
+}
